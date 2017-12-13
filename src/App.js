bcd4415d990c8af00fc613290b59a40fdc837144
Andy Finnell <andy.finnell@gmail.com>
2017-12-13 23:22:31 +0000

    initial layout of portfolio refactor

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import SpaceContainer from "./containers/SpaceContainer/SpaceContainer";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <img id="sig" src="images/sig.png"/>
+                <SpaceContainer/>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+      container
+        planet container
+          turntable
+            planet
+              content
